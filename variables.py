50fefe511dcf4f1205440a641a328cf87220d0b4
Airdropsj <shubhamairdrop07@gmail.com>
2026-06-26 12:59:04 +0530

    in one line multipule variables

--- variables.py
+++ variables.py
@@ -1,2 +1,4 @@
 character_name = "Rakesh chada"
 print(character_name)
+name, age, city = "sj", 25, "pune"
+print(type(name),type(age), type(city)) #it used to check the type of only one variablr at a time
