f72e892b94aee89170a53ebaaea464aaced4dabe
Airdropsj <shubhamairdrop07@gmail.com>
2026-06-26 13:09:23 +0530

    rules of variables

--- variables.py
+++ variables.py
@@ -1,4 +1,9 @@
 character_name = "Rakesh chada"
 print(character_name)
 name, age, city = "sj", 25, "pune"
-print(type(name),type(age), type(city)) #it used to check the type of only one variablr at a time
+print(type(name),type(age), type(city)) #it used to check the type of only one variablr at a time.
+
+# Rules of variables 
+#  1. name starting : always name start with latters and underscore and not start with number
+#  2. case sensitive : uppercase and lowercase are consider different variables.
+#  3. keyword : python has its own words like if, else, for, class etc there are not use as a variables.
