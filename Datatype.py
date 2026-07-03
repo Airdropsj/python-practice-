fe691ec7e7204497addebc0bd5cfb389d8e536e8
Airdropsj <shubhamairdrop07@gmail.com>
2026-07-03 19:43:37 +0530

    data type update

---
+++ Datatype.py
@@ -0,0 +1,58 @@
+                                        # Datatype 
+# Dataype is a classification that defines the kind of value a variable can store the operations that can 
+# be performed on it and how it is represent in memory.
+
+
+#                 classification of datatype
+
+# 1. Numeric type 
+#            int 
+#            float
+#            bool 
+#            complex
+
+# 2. tex type (sequance)
+#            str 
+
+# 3. sequance type 
+#         list 
+#         tuple
+#         range
+
+# 4. set type 
+#        set 
+#        frozenset 
+
+# 5. mapping type
+#        dict 
+
+# 6. binary type 
+#        bytes 
+#        bytearray
+
+# 7. special type 
+#       None
+
+1. Numeric type 
+            
+A. integer (int)
+            immutable datatype,
+it is whole number ,in this not decimal point 
+
+ex. number = 10
+    munber1 = -5
+    number2 = 0
+    number3 = 1502223 these are int
+
+    positive, negative and zero integer
+     10  positive int 
+    -10  negative int
+     0   Zero
+
+no size limit for integer in Python
+ x= 66666666666666666666666666666666666  thats valid 
+
+
+
+
+
