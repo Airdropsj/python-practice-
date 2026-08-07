95861241fcea8a3f1b13cb4fa04a3b984af9ee67
Airdropsj <shubhamairdrop07@gmail.com>
2026-08-07 12:03:28 +0530

    add operatures info

---
+++ operatures.py
@@ -0,0 +1,6 @@
+                                                #   operatures
+# an operator is a symbol or keyword used to perform an operation on values or variables in python.
+total_salary = 10000
+tax_total = 2000
+total_income = total_salary - tax_total 
+print(total_income)
