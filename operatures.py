a6a3ef3cc6d33a2aae10e9c0cdd5d2005c7a5f2e
Airdropsj <shubhamairdrop07@gmail.com>
2026-08-07 12:46:00 +0530

    add type of operator

--- operatures.py
+++ operatures.py
@@ -4,3 +4,62 @@ total_salary = 10000
 tax_total = 2000
 total_income = total_salary - tax_total 
 print(total_income)
+
+# types---
+
+# 1. Arithmetic opeartor
+# 2. Assignment 
+# 3. comparison 
+# 4. logical 
+# 5. other
+
+# 1. arithematic -- this used for mathematical calculation 
+
+# a. Addition (+) :-
+making_cost_toy = 400
+tax_on_toy = 10
+total_cost = making_cost_toy + tax_on_toy
+print(total_cost)
+
+# b. subtraction
+profit_in_share = 100
+loss_in_share = 20
+total_profit = profit_in_share - loss_in_share
+print(total_profit)
+
+# c. multiplication
+quantity_of_share = 50
+price_of_one_share = 20
+total_amount = quantity_of_share*price_of_one_share
+print(total_amount)
+
+# d. division 
+invest_ammount = 500
+share_price = 15
+buy_quantity = invest_ammount/share_price
+print(buy_quantity)
+
+# e. floor division 
+invest_ammountnvest_ammount = 500
+share_price = 15
+buy_quantity_roundfiger = invest_ammount//share_price
+print(buy_quantity_roundfiger)
+
+# f. modulus/remainder
+invest_ammountnvest_ammount = 500
+share_price = 15
+quantity_remaning = invest_ammount%share_price
+print(quantity_remaning)
+
+# g. power
+side = 10
+area = side**2
+print(area)
+# (in 10 square root)/
+
+number = 2
+result = number**3
+print(result)
+# (2 cube)
+
+# 2. Assignment 
